1827225550acd669a0db4a4780b6e81116e4d18b
Tiago Gimenes <tlgimenes@gmail.com>
2018-10-18 23:07:34 -0300

    removing unecessary files

--- node/resolvers/layout.ts
+++ node/resolvers/layout.ts
@@ -1,25 +1,7 @@
 import { map } from 'ramda'
 
 import { CUSTOM_SPEC_FILE, VBASE_BUCKET } from '../common/globals'
-import { getLayoutFromAvailableApps } from '../common/layout'
-import { ignoreNotFound } from '../common/notFound'
 import { spec, specs } from './spec'
-
-interface Args {
-  appName: string
-}
-
-// const normalizeAppName = (appName: string) => appName.replace(/\W/g, '_')
-
-// export const layout = async (root: any, args: Args, ctx: Context, info: any): Promise<Maybe<Array<Maybe<Layout>>>> => {
-//   const {resources: {apps, vbase}}  = ctx
-//   const {appName} = args
-//   const normalizedAppName = normalizeAppName(appName)
-
-//   const customLayout = await vbase.getJSON<Maybe<Layout[]>>('console', normalizedAppName).catch(ignoreNotFound<void>(null))
-
-//   return customLayout ? customLayout : await getLayoutFromAvailableApps(apps)
-// }
 
 interface LayoutArgs {
   appName: string
