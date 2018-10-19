ed3839ee960d5b23ebf2efbb3827366740d7e0a9
luizerasmo <luiz.moreira@vtex.com.br>
2018-10-18 22:20:01 -0300

    Add layout resolvers

--- node/resolvers/layout.ts
+++ node/resolvers/layout.ts
@@ -1,5 +1,9 @@
+import { map } from 'ramda'
+
+import { CUSTOM_SPEC_FILE, VBASE_BUCKET } from '../common/globals'
 import { getLayoutFromAvailableApps } from '../common/layout'
 import { ignoreNotFound } from '../common/notFound'
+import { spec, specs } from './spec'
 
 interface Args {
   appName: string
@@ -21,8 +25,33 @@ interface LayoutArgs {
   appName: string
 }
 
+const getSpecLocatorsFile = (appName: string) => `${appName}.${CUSTOM_SPEC_FILE}`
+
 export const layout = async (root: any, args: LayoutArgs, ctx: Context, info: any) => {
-  return null
+  const { appName } = args
+  const { resources: { vbase }} = ctx
+
+  const appSpecLocatorsFile = getSpecLocatorsFile(appName)
+
+  const maybeAppSpecLocator = await vbase.getJSON(
+    VBASE_BUCKET,
+    appSpecLocatorsFile,
+    true
+  )
+
+  const specLocators = maybeAppSpecLocator && maybeAppSpecLocator.specs
+    || await specs(null, null, ctx, info)
+
+  const layoutArray = await Promise.all(map(
+      (specLocator) => ({
+        spec: spec(null, specLocator, ctx, info),
+        specLocator,
+      }),
+      specLocators
+    )
+  )
+
+  return layoutArray
 }
 
 interface SaveLayoutArgs {
@@ -31,7 +60,25 @@ interface SaveLayoutArgs {
 }
 
 export const saveLayout = async (root: any, args: SaveLayoutArgs, ctx: Context, info: any) => {
-  return null
+  const { appName, specLocators } = args
+  const { resources: { vbase }} = ctx
+
+  const appSpecLocatorsFile = getSpecLocatorsFile(appName)
+
+  try {
+    const response = await vbase.saveJSON(
+      VBASE_BUCKET,
+      appSpecLocatorsFile,
+      {
+        specs: specLocators,
+      }
+    )
+  return true
+  }
+  catch (e) {
+    console.error(e)
+    return false
+  }
 }
 
 interface ResetLayoutArgs {
@@ -39,5 +86,20 @@ interface ResetLayoutArgs {
 }
 
 export const resetLayout = async (root: any, args: SaveLayoutArgs, ctx: Context, info: any) => {
-  return null
+  const { appName, specLocators } = args
+  const { resources: { vbase }} = ctx
+
+  const appSpecLocatorsFile = getSpecLocatorsFile(appName)
+
+  try {
+    await vbase.deleteFile(
+      VBASE_BUCKET,
+      appSpecLocatorsFile
+    )
+    return true
+  }
+  catch (e) {
+    console.error(e)
+    return false
+  }
 }
