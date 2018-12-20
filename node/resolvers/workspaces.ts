b0dfefc0444d1bbe4f2952987c5fcaebc7b7a5b9
germanohn <germano.huning@vtex.com>
2018-12-20 15:58:20 -0200

    Test

--- node/resolvers/workspaces.ts
+++ node/resolvers/workspaces.ts
@@ -2,16 +2,27 @@ import { Workspaces } from '@vtex/api'
 
 export const workspaces = async (root, args, ctx: Context, info) => {
   const {vtex} = ctx
-  const availableWorkspaces : WorkspaceResponse[] = []
-  const workspacesArray = new Workspaces(vtex, {})
-  await workspacesArray.list(vtex.account)
-    .then((workspaceArray: WorkspaceResponse[]) =>
-      workspaceArray.forEach(workspace => {
-        const name = workspace.name
-        const weight = workspace.weight
-        const production = workspace.production
-        availableWorkspaces.push({name, weight, production})
-      })
-  )
+  const availableWorkspaces : WorkspaceResponse[] = [
+    {
+      name: 'primeiro',
+      weight: 1,
+      production: true,
+    },
+    {
+      name: 'segundo',
+      weight: 2,
+      production: true,
+    }
+  ]
+  // const workspacesArray = new Workspaces(vtex)
+  // await workspacesArray.list(vtex.account)
+  //   .then((workspaceArray: WorkspaceResponse[]) =>
+  //     workspaceArray.forEach(workspace => {
+  //       const name = workspace.name
+  //       const weight = workspace.weight
+  //       const production = workspace.production
+  //       availableWorkspaces.push({name, weight, production})
+  //     })
+  // )
   return availableWorkspaces
 }
