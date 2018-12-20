6aa18af7081f75e83567db980abdee57cbac2eb6
germanohn <germano.huning@vtex.com>
2018-12-20 16:41:44 -0200

    Add policy to allow request for workspaces, update version of vtex/api and working in workspaces.ts

--- node/resolvers/workspaces.ts
+++ node/resolvers/workspaces.ts
@@ -1,28 +1,34 @@
 import { Workspaces } from '@vtex/api'
+import { map, prop } from 'ramda'
 
 export const workspaces = async (root, args, ctx: Context, info) => {
   const {vtex} = ctx
-  const availableWorkspaces : WorkspaceResponse[] = [
-    {
-      name: 'primeiro',
-      weight: 1,
-      production: true,
-    },
-    {
-      name: 'segundo',
-      weight: 2,
-      production: true,
-    }
-  ]
-  // const workspacesArray = new Workspaces(vtex)
-  // await workspacesArray.list(vtex.account)
-  //   .then((workspaceArray: WorkspaceResponse[]) =>
-  //     workspaceArray.forEach(workspace => {
-  //       const name = workspace.name
-  //       const weight = workspace.weight
-  //       const production = workspace.production
-  //       availableWorkspaces.push({name, weight, production})
-  //     })
-  // )
-  return availableWorkspaces
+  const availableWorkspaces : WorkspaceResponse[] = []
+  // const availableWorkspaces : WorkspaceResponse[] = [
+  //   {
+  //     name: 'primeiro',
+  //     weight: 1,
+  //     production: true,
+  //   },
+  //   {
+  //     name: 'segundo',
+  //     weight: 2,
+  //     production: true,
+  //   }
+  // ]
+  const workspacesArray = new Workspaces(vtex)
+  await workspacesArray.list(vtex.account)
+    .then((workspaceArray: WorkspaceResponse[]) =>
+      workspaceArray.forEach(workspace => {
+        const name = workspace.name
+        const weight = workspace.weight
+        const production = workspace.production
+        availableWorkspaces.push({name, weight, production})
+      })
+  )
+  console.log('availableWorkspaces\n', availableWorkspaces, '\navailableWorkspaces')
+  //return availableWorkspaces
+  const result = map(prop('name'))(availableWorkspaces)
+  console.log('result\n', result, '\nresult')
+  return result
 }
