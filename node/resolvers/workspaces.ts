9862fe5309f64edd93850bf90fb59e06e2f72f70
germanohn <germano.huning@vtex.com>
2018-12-21 11:12:58 -0200

    Dropdown with workspaces
    1) remove workspaceResponse from schema
    2) refactor code of workspaces and appsWithStats resolvers

--- node/resolvers/workspaces.ts
+++ node/resolvers/workspaces.ts
@@ -1,34 +1,15 @@
 import { Workspaces } from '@vtex/api'
-import { map, prop } from 'ramda'
+
 
 export const workspaces = async (root, args, ctx: Context, info) => {
   const {vtex} = ctx
-  const availableWorkspaces : WorkspaceResponse[] = []
-  // const availableWorkspaces : WorkspaceResponse[] = [
-  //   {
-  //     name: 'primeiro',
-  //     weight: 1,
-  //     production: true,
-  //   },
-  //   {
-  //     name: 'segundo',
-  //     weight: 2,
-  //     production: true,
-  //   }
-  // ]
+  const availableWorkspaces : string[] = []
   const workspacesArray = new Workspaces(vtex)
   await workspacesArray.list(vtex.account)
     .then((workspaceArray: WorkspaceResponse[]) =>
       workspaceArray.forEach(workspace => {
-        const name = workspace.name
-        const weight = workspace.weight
-        const production = workspace.production
-        availableWorkspaces.push({name, weight, production})
+        availableWorkspaces.push(workspace.name)
       })
   )
-  console.log('availableWorkspaces\n', availableWorkspaces, '\navailableWorkspaces')
-  //return availableWorkspaces
-  const result = map(prop('name'))(availableWorkspaces)
-  console.log('result\n', result, '\nresult')
-  return result
+  return availableWorkspaces
 }
