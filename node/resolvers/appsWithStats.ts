85c55496cf019912226ebdddce568d3cc45e71a8
germanohn <germano.huning@vtex.com>
2019-01-04 15:05:29 -0200

    Made suggested changes in AppName filter

--- node/resolvers/appsWithStats.ts
+++ node/resolvers/appsWithStats.ts
@@ -1,15 +1,26 @@
 import { filter, includes, map, pluck, prop } from 'ramda'
 
 
+const filterAppNameByAccount = (appNameList: string[], accountName: string) => {
+  const isVendorInApp = (appName: string) => {
+    const appNamePart: string[] = appName.split('.')
+    return includes(accountName, appNamePart[0])
+  }
+  return filter(isVendorInApp, appNameList)
+}
+
+const removeAccountInAppName = (appNameList: string[], accountName: string) => {
+  const accountNameLength = accountName.length
+  return map(str => str.substring(accountNameLength + 1), appNameList)
+}
+
 export const appsWithStats = async (root, args, ctx: Context, info) => {
   const {dataSources: {registry}, vtex: {account}} = ctx
   const publishedAppsInThisAccount = await registry.listApps()
     .then(prop('data'))
     .then(pluck('partialIdentifier'))
-    .then(result => filter(includes(account), result))
-    .then(result => {
-      const accountStringLength = account.length
-      return map(str => str.substring(accountStringLength + 1), result)})
+    .then(result => filterAppNameByAccount(result, account))
+    .then(result => removeAccountInAppName(result, account))
     .then(result => result.sort())
 
   return publishedAppsInThisAccount
