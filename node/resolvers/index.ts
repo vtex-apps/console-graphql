56d506d9d609cd79421086cb78a7acfe7b075a2e
germanohn <germano.huning@vtex.com.br>
2019-03-01 10:48:39 -0300

    Add appVersions resolver

--- node/resolvers/index.ts
+++ node/resolvers/index.ts
@@ -1,8 +1,10 @@
 import { appsWithStats } from './appsWithStats'
+import { appVersions } from './appVersions'
 import { data } from './data'
 
 export const resolvers = {
   Query: {
+    appVersions,
     appsWithStats,
     data,
   },
